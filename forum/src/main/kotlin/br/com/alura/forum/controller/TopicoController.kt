e33be86ae97e8a08501518c6e7621990eb240ea0
Paulo Patrício <paulo.patricio21@gmail.com>
2023-01-14 16:53:34 -0300

    Criação do endpoint para cadastrar um topico

--- forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -6,6 +6,8 @@ import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.Arrays
@@ -22,6 +24,11 @@ class TopicoController (private val service: TopicoService) {
     @GetMapping("/{id}")
     fun buscarPorId(@PathVariable id: Long): Topico {
         return service.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody topico: Topico) {
+        service.cadastrar(topico)
     }
 
 }
--- forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -5,60 +5,10 @@ import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
 import java.util.*
+import kotlin.collections.ArrayList
 
 @Service //Com esta anotação é possível injetar esta classe em qualquer outra classe gerenciada pelo Spring
-class TopicoService(private var topicos: List<Topico>) {
-
-    init {
-        val topico = Topico(
-            id = 1,
-            titulo = "Duvida Kotlin",
-            mensagem = "Variáveis no Kotlin",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Paulo Patricio",
-                email= "paulo@email.com"
-            )
-        )
-
-        val topico2 = Topico(
-            id = 2,
-            titulo = "Duvida Kotlin 2",
-            mensagem = "Variáveis no Kotlin 2",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Paulo Patricio",
-                email= "paulo@email.com"
-            )
-        )
-
-        val topico3 = Topico(
-            id = 3,
-            titulo = "Duvida Kotlin 3",
-            mensagem = "Variáveis no Kotlin 3",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Paulo Patricio",
-                email= "paulo@email.com"
-            )
-        )
-        topicos = listOf(topico, topico2, topico3)
-    }
+class TopicoService(private var topicos: List<Topico> = ArrayList()) {
 
     fun listar(): List<Topico> {
         return topicos
@@ -68,5 +18,9 @@ class TopicoService(private var topicos: List<Topico>) {
         return topicos.stream().filter { t ->
             t.id == id //dado o topico (t), eu quero o topico que tiver o id que foi passado no parâmetro
         }.findFirst().get()
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos.plus(topico)
     }
 }
