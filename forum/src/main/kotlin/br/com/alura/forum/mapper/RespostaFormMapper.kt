f1c479de3b111ac7d071d18d9cc6a0bddb0a153c
Paulo Patrício <paulo.patricio21@gmail.com>
2023-01-19 18:12:03 -0300

    Adição da anotação compenent nas classes mappers para o Spring compilar as classes corretamente

--- forum/src/main/kotlin/br/com/alura/forum/mapper/RespostaFormMapper.kt
+++ forum/src/main/kotlin/br/com/alura/forum/mapper/RespostaFormMapper.kt
@@ -4,7 +4,9 @@ import br.com.alura.forum.dto.NovaRespostaForm
 import br.com.alura.forum.model.Resposta
 import br.com.alura.forum.service.TopicoService
 import br.com.alura.forum.service.UsuarioService
+import org.springframework.stereotype.Component
 
+@Component
 class RespostaFormMapper(
     private val usuarioService: UsuarioService,
     private val topicoService: TopicoService
--- forum/src/main/kotlin/br/com/alura/forum/mapper/RespostaViewMapper.kt
+++ forum/src/main/kotlin/br/com/alura/forum/mapper/RespostaViewMapper.kt
@@ -2,7 +2,9 @@ package br.com.alura.forum.mapper
 
 import br.com.alura.forum.dto.RespostaView
 import br.com.alura.forum.model.Resposta
+import org.springframework.stereotype.Component
 
+@Component
 class RespostaViewMapper: Mapper<Resposta, RespostaView> {
 
     override fun map(resposta: Resposta): RespostaView {
