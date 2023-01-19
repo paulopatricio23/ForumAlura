52602ef2edb4560ed443b98f6b2807d9901d3a61
Paulo Patrício <paulo.patricio21@gmail.com>
2023-01-19 17:57:41 -0300

    Criação dos endpoints para atualizar e excluir topicos

--- forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,12 +1,15 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -28,6 +31,16 @@ class TopicoController (private val service: TopicoService) {
     @PostMapping
     fun cadastrar(@RequestBody @Valid novoTopicoForm: NovoTopicoForm) {
         service.cadastrar(novoTopicoForm)
+    }
+
+    @PutMapping
+    fun atualizar(@RequestBody @Valid atualizacaoTopicoForm: AtualizacaoTopicoForm) {
+        service.atualizar(atualizacaoTopicoForm)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletar(@PathVariable id: Long) {
+        service.deletar(id)
     }
 
 }
--- forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.mapper.TopicoFormMapper
@@ -39,5 +40,28 @@ class TopicoService(
         val topico = topicoFormMapper.map(novoTopicoForm)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+    }
+
+    fun atualizar(atualizacaoTopicoForm: AtualizacaoTopicoForm) {
+        val topico = topicos.stream().filter { t ->
+            t.id == atualizacaoTopicoForm.id //dado o topico (t), eu quero o topico que tiver o id que foi passado no parâmetro
+        }.findFirst().get()
+        topicos = topicos.minus(topico).plus(Topico(
+            id = atualizacaoTopicoForm.id,
+            titulo = atualizacaoTopicoForm.titulo,
+            mensagem = atualizacaoTopicoForm.mensagem,
+            autor = topico.autor,
+            curso = topico.curso,
+            respostas = topico.respostas,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        ))
+    }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter { t ->
+            t.id == id //dado o topico (t), eu quero o topico que tiver o id que foi passado no parâmetro
+        }.findFirst().get()
+        topicos = topicos.minus(topico)
     }
 }
