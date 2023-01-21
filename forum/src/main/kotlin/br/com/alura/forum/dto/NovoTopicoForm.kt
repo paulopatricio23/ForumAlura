3fe97c4ba61362458a046a72e3eb7c07053718d3
Paulo Patrício <paulo.patricio21@gmail.com>
2023-01-21 17:10:18 -0300

    Adicionando tratamento de erros do bean validation com mensagens personalizadas

--- forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class NovoTopicoForm(
-    @field:NotEmpty
-    @field:Size(min = 5, max = 100)
+    @field:NotEmpty (message = "Título não pode ser em branco")
+    @field:Size(min = 5, max = 100, message = "Título deve ter entre 5 e 100 caracteres")
     val titulo: String, //As anotações do bean validation deveriam estar no método get, mas como estão no construtor, precisa colocar a anotation @field
-    @field:NotEmpty
+    @field:NotEmpty (message = "Mensagem não pode ser em branco")
     val mensagem: String,
     @field:NotNull
     val idCurso: Long,
--- forum/src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
+++ forum/src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.exception
 import br.com.alura.forum.dto.ErrorView
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -27,13 +28,32 @@ class ExceptionHandler {
     @ExceptionHandler(Exception::class) // Informa ao Spring que quando qualquer exception acontecer, este método será executado
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // O ResponseStatus a ser devolvido
     fun handleServerError(
-        exception: NotFoundException,
+        exception: Exception,
         request: HttpServletRequest
     ): ErrorView {
         return ErrorView(
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class) // Informa ao Spring que quando a exception MethodArgumentNotValidException (erro de validação do bean) acontecer, este método será executado
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // O ResponseStatus a ser devolvido
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest,
+    ): ErrorView {
+        val errorMessage = HashMap<String, String?>() // Variável que vai conter um map com todos os erros de forma simplificada
+        exception.bindingResult.fieldErrors.forEach{ // Percorrer todos os erros vindos na exception
+            e ->
+            errorMessage[e.field] = e.defaultMessage // Adicionando apenas a mensagem do erro. Esta mensagem pode ser personalizada na configuração do bean no data class
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
             path = request.servletPath
         )
     }
